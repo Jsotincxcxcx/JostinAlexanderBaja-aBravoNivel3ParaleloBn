84268d033e95233183ad2469543987b5f6cc0852
jostin <jostin@LAPTOP-TE1QSLUF>
2021-10-30 16:35:18 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JostinAlexanderBajañaBravoNivel3ParaleloBn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, Mayor = 0, Numeros;
+            for(i=0; i < 5; i++)
+            {
+                Console.WriteLine(" Por favor Digite el número" );
+                Numeros = int.Parse(Console.ReadLine());
+                if (Numeros > Mayor)
+                {
+                    Mayor = Numeros;
+                }
+                
+            }
+            Console.WriteLine(" El numero mayor es " + Mayor);
+            Console.ReadKey();
+        }
+    }
+}
