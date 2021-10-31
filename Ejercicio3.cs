dd65af9dacd5d5a12dccfe895d72e6bfc1391aaa
Jsotincxcxcx <91404287+Jsotincxcxcx@users.noreply.github.com>
2021-10-31 13:31:05 -0500

    Add files via upload

---
+++ Ejercicio3.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio3
+{
+    class Ejercicio3
+    {
+        static void Main(string[] args)
+        {
+            int Cantidad;
+            double Precios;
+            double Total ;
+            double Contador = 0;
+            Console.WriteLine(" Por favor introduzca la cantidad vendida");
+            Cantidad = int.Parse(Console.ReadLine()); 
+            if (Cantidad > 0)
+            {
+                for (int i = 1; i <= Cantidad; i++)
+                {
+                    Console.WriteLine(" Por favor ingrese el precio ");
+                    Precios = double.Parse(Console.ReadLine());
+                    Contador = Precios + Contador;
+                }
+            }          
+            Total = Contador;           
+            Console.WriteLine(" El total de la factura es : " + Total);
+            Console.WriteLine(" Vuelva pronto cabeza de tula * Lo queremos :) * ");
+          Console.ReadKey();
+        }
+        
+    }
+}
