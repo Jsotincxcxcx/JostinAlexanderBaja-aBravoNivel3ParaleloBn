38756d12e318a88e9975e26a1805ad837781eeb4
Jsotincxcxcx <91404287+Jsotincxcxcx@users.noreply.github.com>
2021-10-30 23:46:43 -0500

    Add files via upload

---
+++ Ejercicioo2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejerci2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Numero;
+            Console.WriteLine(" Por favor Ingrese un número");
+            Numero = int.Parse(Console.ReadLine());
+            for (int i= 1;i<= Numero; i++)
+            {
+                for (int Linea = 1; Linea <= Numero; Linea++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine(" ");
+
+
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
